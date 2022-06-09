157e0e18c9ac8f368dcfc21794ee6f2dcaf25c05
CoriWright <corinne.r.wright@gmail.com>
2022-06-09 13:46:57 -0400

    ThirdIteration

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
+# Readme
 # Enverus
 Enverus Take Home
 
 ## Code Walkthrough
+I used python and the pandas library to complete the work.  I have commented the python file to explain each step.  The final dataset is named “final_playstore_ratings.csv” .  I have also kept files created along the way for reasoning purposes.
+
 ### Question 1
 
+> Filter the dataset to have 
+>     1. one entry per App name 
+>     2. remove apps with invalid ratings
+
+1. I de-duped the dataset and put it into the “one_app” data frame.
+2. I noticed that apps like “8 Ball Pool” still had 7 entries because each record had a unique number of ratings, and one had a different category than the rest
+	1. **Assumption** Since the number of ratings were so similar, I  assumed that the record was pulled multiple times at different times.
+	2. To solve this, I created one record that had the highest frequency of category and the max amount of ratings .
+3. I took the last dataset and removed the invalid ratings by removing null values.  This field has a datatype of float64 so if it were not a number, the record would be invalid.
+
 ### Question 2
+> Add a new gross revenue feature for each app 
+
 
 ### Question 3
+> Return the top 3 grossing apps in each category
--- googleplay.py
+++ googleplay.py
@@ -1,15 +1,44 @@
 import csv
 import pandas as pd
 
-
+# Initial dataframe of entire file
 playstore = pd.read_csv("googleplaystore.csv")
+
+# De-duped the file
 one_app = playstore.drop_duplicates()
-remove_invalid_rating = one_app[(one_app["Rating"].notna())]
-print(remove_invalid_rating)
+
+#test 8 Ball Pool
+eightball = one_app[one_app["App"] == "8 Ball Pool"]
+aggregations = {
+    'Reviews':'max',
+    'Category': lambda x: x.value_counts().index[0]
+    
+}
+#eightball1 = eightball.groupby(['App','Rating','Size','Installs','Type','Price','Content Rating','Genres','Last Updated','Current Ver','Android Ver']).agg(aggregations)
+#print(eightball1)
+#eightball1 = eightball.groupby(['App','Rating','Size','Installs','Type','Price','Content Rating','Reviews','Genres','Last Updated','Current Ver','Android Ver'])
+#'Category'].apply(lambda x: x.value_counts().index[0]).reset_index()
+one_app_agg = one_app.groupby(['App','Rating','Size','Installs','Type','Price','Content Rating','Genres','Last Updated','Current Ver','Android Ver']).agg(aggregations)
+print(one_app_agg)
+app_count = one_app_agg.groupby('App').agg('count')
+print(app_count)
+app_count.to_csv("agg_count.csv")
+one_app_agg.to_csv("one_app_agg.csv")
+
+
+'''
+# Aggregate out duplicate app names by frequency of Category and Largest number of Ratings
+aggregation_functions = {'Reviews': 'max'}
+one_app = one_app.groupby(one_app['App']).agg(aggregation_functions)
 print(one_app)
 one_app.to_csv("dedupe.csv")
 
+remove_invalid_rating = one_app[(one_app["Rating"].notna())]
+print(remove_invalid_rating)xs
+print(one_app)
+one_app.to_csv("dedupe.csv")
 
+'''
 #8 Ball Pool
 
 
