6ff437ae735bb7c70575dc70f98350c0f4013c35
CoriWright <corinne.r.wright@gmail.com>
2022-06-09 02:05:33 -0400

    SecondIteration

--- googleplay.py
+++ googleplay.py
@@ -1,1 +1,21 @@
-print("yo momma")
+import csv
+import pandas as pd
+
+
+playstore = pd.read_csv("googleplaystore.csv")
+one_app = playstore.drop_duplicates()
+remove_invalid_rating = one_app[(one_app["Rating"].notna())]
+print(remove_invalid_rating)
+print(one_app)
+one_app.to_csv("dedupe.csv")
+
+
+#8 Ball Pool
+
+
+
+
+
+
+#filtered = filter(lambda p: 'Category' == "ART_AND_DESIGN", reader) 
+#csv.writer(open(r"playstore_filtered.csv",'w'),delimiter=',').writerows(filtered)
